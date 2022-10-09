302beed7151935f37b0184d08d1b76a7c01a79bc
vitalii <vitalik.yesyp@gmail.com>
2022-10-10 02:26:35 +0300

    Create quotes

--- src/components/app-quotes/app-quotes.js
+++ src/components/app-quotes/app-quotes.js
@@ -1,7 +1,53 @@
-const AppQuotes = () => {
-    <div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import imgAvatar from '../../img/avatar.png'
 
-    </div>
+import './app-quotes.css'
+
+const AppQuotes = () => {
+    return (
+        <div className="quotes">
+            <div className="container-quotes">
+                <div /*id="slider-btn-prev"*/ className="slider-btn prev">
+                    <i><FontAwesomeIcon icon={faAngleLeft} className='i-a-f'/></i>
+                </div>
+                <div className="quotes-inner slider-container">
+                    <div data-slider="1" className="slider-track">
+                        <div className="quotes-item">
+                            <img src={imgAvatar} alt="" className="quotes-item-img"/>
+                            <div className="quotes-item-text">
+                            <p className="quotes-item-quote">
+                            "Lorem ipsum dolor sit amet consectetur adipisicing, elit. Quis, pariatur iusto commodi beatae suscipit voluptatum laboriosam unde vitae voluptates nam aliquid aut animi voluptatibus libero et blanditiis."
+                            </p>
+                            <span className="quotes-item-author">Jon Doe</span>
+                        </div>
+                        </div>
+                        {/* <div className="quotes-item">
+                            <img src={imgAvatar} alt="" className="quotes-item-img"/>
+                            <div className="quotes-item-text">
+                            <p className="quotes-item-quote">
+                            "Lorem ipsum dolor sit amet consectetur adipisicing, elit. Quis, pariatur iusto commodi beatae suscipit voluptatum laboriosam unde vitae voluptates nam aliquid aut animi voluptatibus libero et blanditiis."
+                            </p>
+                            <span className="quotes-item-author">Jacob Doe</span>
+                        </div>
+                        </div>
+                        <div className="quotes-item">
+                            <img src={imgAvatar} alt="" className="quotes-item-img"/>
+                            <div className="quotes-item-text">
+                            <p className="quotes-item-quote">
+                            "Lorem ipsum dolor sit amet consectetur adipisicing, elit. Quis, pariatur iusto commodi beatae suscipit voluptatum laboriosam unde vitae voluptates nam aliquid aut animi voluptatibus libero et blanditiis."
+                            </p>
+                            <span className="quotes-item-author">Joseph Doe</span>
+                        </div>
+                        </div> */}
+                    </div>
+                </div>
+                <div /*id="slider-btn-next"*/ className="slider-btn next">
+                    <i><FontAwesomeIcon icon={faAngleRight} className='i-a-f'/></i>
+                </div>
+            </div>
+      </div>
+    );
 }
 
 export default AppQuotes;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,6 +4,7 @@ import StatistickItem from "../statistick-item/statistick-item";
 import AppSection1 from "../app-section1/app-section1";
 import AppSection2 from "../app-section2/app-section2";
 import AppSection3 from "../app-section3/app-section3";
+import AppQuotes from "../app-quotes/app-quotes";
 
 import './app.css'
 
@@ -16,6 +17,7 @@ function App(){
             <AppSection1/>
             <AppSection2/>
             <AppSection3/>
+            <AppQuotes/>
         </div>
     );
 }
